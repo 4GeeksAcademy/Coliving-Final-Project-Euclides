f995c69cda94bb682529e20aaaf364bb97e2a41b
EuclidesT <94712951+EuclidesT@users.noreply.github.com>
2024-10-05 14:21:48 +0000

    Perfil de Usuario

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,12 +1,12 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import Layout from "./layout.js";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
 //import your own components
-import Publish from "./pages/formAD.jsx";
 
 //render your react application
-ReactDOM.render(<Publish />, document.querySelector("#app"));
+ReactDOM.render(<Layout />, document.querySelector("#app"));
